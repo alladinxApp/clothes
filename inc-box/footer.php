f2dad26f861b97e16f3e83400a6b05bce5a1a009
Rey P. Castanares <alladinx@gmail.com>
2016-06-13 01:06:04 +0800

    commit initial design for clothes station

---
+++ inc-box/footer.php
@@ -0,0 +1,6 @@
+<div class="clearfix"></div>
+<footer>
+<p>
+	<span style="text-align:left;float:left">&copy; 2016 <a href="http://www.clothes-station.com/" alt="Clothes Station" target="_blank">Clothes Station.</a> All Rights Reserved.</span>
+</p>
+</footer>
